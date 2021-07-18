c7c64ae8204d9c52268637e934e0cf0819f4b3f7
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-18 17:59:52 +0300

    create a login page for the user in the project

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -3,4 +3,7 @@ from .import views
 
 urlpatterns=[
     url('^$',views.home,name = 'home'),
+    url('register/', views.registerPage, name = 'register'),
+    url('login/', views.loginPage, name = 'login'),
+    # url('logout/', views.logoutUser, name = 'logout'),
 ]
--- myawwards/views.py
+++ myawwards/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render, redirect
-
+from django.http import HttpResponse
+from django.contrib.auth.forms import UserCreationForm
+from .forms import CreateUserForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def registerPage(request):
+    form= CreateUserForm
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Welcome' + user)
+            return redirect('login')
+    context={'form': form}
+    return render(request,'register.html', context)
+
+def loginPage(request):
+    if request.method == 'POST':
+        username =request.POST.get('username')
+        password =request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+    context={}
+    return render(request, 'login.html', context)
