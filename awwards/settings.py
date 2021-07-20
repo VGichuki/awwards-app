0ab8c74df10eede761f2cc87cd00c7fb8c1b1b67
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-20 22:48:59 +0300

    install pillow for the imagefield

--- awwards/settings.py
+++ awwards/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -130,13 +131,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from django.conf.urls import url
 from .import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns=[
@@ -14,3 +16,5 @@ urlpatterns=[
     path('post/', views.post_project, name='new_post'),
     # url(r'^search/', views.search, name='search')
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
