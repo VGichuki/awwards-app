3d5a73467eabcdc325aff789efbafbb3a71aad47
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-20 22:36:47 +0300

    create a url route for profile

--- myawwards/urls.py
+++ myawwards/urls.py
@@ -1,12 +1,16 @@
+from django.urls import path
 from django.conf.urls import url
 from .import views
 
+
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
-    url('register/', views.registerPage, name = 'register'),
-    url('login/', views.loginPage, name = 'login'),
-    url('logout/', views.logoutUser, name = 'logout'),
+    path('',views.home,name = 'home'),
+    path('register/', views.registerPage, name = 'register'),
+    path('login/', views.loginPage, name = 'login'),
+    path('logout/', views.logoutUser, name = 'logout'),
     url('profile/', views.user_profile, name='profile'),
     url('profile/<username>/', views.update_user_profile, name='userprofile'),
+    url('profile/<username>/edit/', views.edit_user_profile, name='editprofile'),
+    path('post/', views.post_project, name='new_post'),
     # url(r'^search/', views.search, name='search')
 ]
