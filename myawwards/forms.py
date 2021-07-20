581b7fa1d9df623b597aba3d78a7182d5846784a
VGichuki <vivian.gichuki@student.moringaschool.com>
2021-07-20 22:37:27 +0300

    create a form for updating a user bio

--- myawwards/forms.py
+++ myawwards/forms.py
@@ -1,8 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from .models import Project,Profile
+from pyuploadcare.dj.forms import ImageField
 
 class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+class UpdateUserForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class UpdateProfileForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['user', 'profile_pic', 'bio', 'contact']
+
+class PostForm(forms.ModelForm):
+    image = ImageField(label='')
+    class Meta:
+        model = Project
+        fields = ['title', 'image', 'url', 'description']
